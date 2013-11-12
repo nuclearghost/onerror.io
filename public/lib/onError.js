f8bfbb162850346d4d191c357d6e7a9147ce4f5d
Mark Meyer <mark.meyer37@gmail.com>
2013-11-11 20:26:50 -0600

    require an API key, handle more situations

--- public/lib/onError.js
+++ public/lib/onError.js
@@ -1,18 +1,37 @@
 (function(){
-	window.onerror = function(message, url, linenumber){
-		if (window.XMLHttpRequest){
-			var xhr = new XMLHttpRequest();
-			var postUrl = "http://localhost:3000/errors";
-			var log = url + ":" + linenumber + " " + message;
+	var scripts = document.getElementsByTagName('script'),
+		script = scripts[scripts.length - 1],
+		apiKey = script.getAttribute('data-key'),
+		propogateError = script.getAttribute('data-propogate');
 
-			xhr.open("POST", postUrl);
-			xhr.setRequestHeader("Content-Type", "application/json");
-			xhr.send(JSON.stringify({ url: url,
-				lineNumber: linenumber,
-				message: message
-			}));
+	if (apiKey) {
+		window.onerror = function(message, url, linenumber){
+			if (window.XMLHttpRequest){
+				var xhr = new XMLHttpRequest(),
+					postUrl = "http://localhost:3000/errors",
+					log = url + ":" + linenumber + " " + message;
+
+				xhr.open("POST", postUrl);
+				xhr.setRequestHeader("Content-Type", "application/json");
+
+				if (message && url && linenumber) {
+					xhr.send(JSON.stringify({ 
+						message: message,
+						url: url,
+						lineNumber: linenumber,
+						key: apiKey
+					}));
+				} else {
+					xhr.send(JSON.stringify({ 
+						message: arguments[0],
+						url: arguments[1],
+						lineNumber: arguments[2],
+						key: apiKey
+					}));
+				}
+			}
+			return false;
 		}
-		return false;
 	}
 
 })();
